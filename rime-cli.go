2420eda4cbbeeddf1550ed3040040bb8ff475db8
jiefeng <jiefeng.wang@definesys.com>
2023-07-27 12:08:09 +0800

    feat(us-0001): 添加根据编码搜索功能

--- rime-cli.go
+++ rime-cli.go
@@ -90,8 +90,8 @@ func delByCode(customCode string) {
 	fmt.Print(errorInfo)
 }
 
-//通过编码搜索该字符是否存在
-//TODO
+// 通过编码搜索该字符是否存在
+// TODO
 func searchCode(code string) {
 	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
 	defer file.Close()
