7152d5696901681bd5d0b2db31327a19d0c005ca
jiefeng.wang <51876176+puddinging@users.noreply.github.com>
2023-08-04 21:54:36 +0800

    :sparkles:(更新一下): 更新code

--- function.go
+++ function.go
@@ -1,6 +1,10 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 /*
 *
@@ -9,4 +13,12 @@ import "strings"
 func Trim(str string) string {
 	print(str)
 	return strings.Trim(str, "")
+}
+
+func updateCode(code string) {
+	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
+	defer file.Close()
+	if err == nil {
+		fmt.Print("更新成功")
+	}
 }
