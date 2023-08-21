97fb3f9104e60fabc7b6f44da066a0d0c3e0211c
jiefeng.wang <jiefeng.wang@outlook.com>
2023-08-21 15:28:55 +0800

    feature(us-0001): 添加执行系统命令的方法

---
+++ command.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func exeCommand(command string) {
+	cmd := exec.Command("ls")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
