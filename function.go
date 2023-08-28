b12835cba10dd3ddd4b3fae14475521fdb1fbc27
jiefeng.wang <jiefeng.wang@outlook.com>
2023-08-28 10:01:07 +0800

    feature(): 添加一些字符串函数

--- function.go
+++ function.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"strings"
 )
 
@@ -15,107 +13,16 @@ func Trim(str string) string {
 	return strings.Trim(str, "")
 }
 
-func updateCode(code string) {
-	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
-	defer file.Close()
-	if err == nil {
-		fmt.Print("更新成功")
-	}
+/**
+* 字符串判断为空
+ */
+func isEmpty(str string) bool {
+	return len(str) == 0
 }
 
-func deleteCode(code string) {
-	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
-	defer file.Close()
-	if err == nil {
-		fmt.Print("更新成功")
-	}
-}
-
-func complateCode(code string) {
-	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
-	defer file.Close()
-	if err == nil {
-		fmt.Print("更新成功")
-	}
-}
-
-func testCode(code string) {
-	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
-	defer file.Close()
-	if err == nil {
-		fmt.Print("更新成功")
-		// 你好
-	}
-}
-
-func test5Code(code string) {
-	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
-	defer file.Close()
-	if err == nil {
-		fmt.Print("更新成功")
-		// 你好
-	}
-}
-
-func test6Code(code string) {
-	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
-	defer file.Close()
-	if err == nil {
-		fmt.Print("更新成功")
-		// 你好
-	}
-}
-
-func print(code string) {
-	file, err := os.OpenFile(*filePath, os.O_APPEND|os.O_WRONLY, 0644)
-	defer file.Close()
-	if err == nil {
-		fmt.Print("更新成功")
-		// 你好
-	}
-}
-
-func test(str string) string {
-	print(str)
-	return strings.Trim(str, "")
-}
-
-func query(str string) string {
-	print(str)
-	return strings.Trim(str, "")
-}
-
-func test1(str string) string {
-	print(str)
-	return strings.Trim(str, "")
-}
-
-func test2(str string) string {
-	print(str)
-	return strings.Trim(str, "")
-}
-
-func test3(str string) string {
-	print(str)
-	return strings.Trim(str, "")
-}
-
-func test4(str string) string {
-	print(str)
-	return strings.Trim(str, "")
-}
-
-func test7(str string) string {
-	print(str)
-	return strings.Trim(str, "")
-}
-
-func test8(str string) string {
-	print(str)
-	return strings.Trim(str, "")
-}
-
-func test9(str string) string {
-	print(str)
-	return strings.Trim(str, "")
+/**
+* 字符串判断非空
+ */
+func isNotEmpty(str string) bool {
+	return !isEmpty(str)
 }
